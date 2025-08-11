2b40039c6c38b7296c7f8d10d93086371684017d
Rohit Bhalerao <rohitbhalerao@rohits-mbp.home>
2025-08-11 20:32:01 +0200

    App code fixes

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ if st.checkbox("Show Data", False):
 l1 = data[['Order_ID', 'DATE', 'DAY','TIME','TOTAL','WEEK NUMBER']]
 l2 = data['Order_ID'].nunique()
 groupl = l1.groupby('Order_ID')
-l3 = groupl['TOTAL'].agg([np.sum])
+l3 = groupl['TOTAL'].agg(['sum'])
 groupl2 = l1.groupby('DATE')
-l4 = groupl2['TOTAL'].agg([np.sum])
+l4 = groupl2['TOTAL'].agg(['sum'])
 groupl3 = l1.groupby('WEEK NUMBER')
-l5 = groupl3['TOTAL'].agg([np.sum])
+l5 = groupl3['TOTAL'].agg(['sum'])
 groupl4 = l1.groupby('DAY')
-l6 = groupl4['TOTAL'].agg([np.sum])
+l6 = groupl4['TOTAL'].agg(['sum'])
 l6 = l6.loc[['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'], :]
 l6['Cumulative Revenue'] = l6['sum'].cumsum()
 l6['Cumulative Percentage'] = 100*l6['Cumulative Revenue']/l6['sum'].sum()
@@ -57,7 +57,6 @@ ax.set(xlabel="Date",
 
 # Rotate tick marks on x-axis
 plt.setp(ax.get_xticklabels(), rotation=45)
-plt.show()
 
 
 fig2, ax = plt.subplots(figsize=(10, 10))
@@ -74,7 +73,6 @@ ax.set(xlabel="Week of the month",
 
 # Rotate tick marks on x-axis
 plt.setp(ax.get_xticklabels(), rotation=45)
-plt.show()
 
 fig3, ax = plt.subplots(figsize=(10, 10))
 
@@ -91,12 +89,10 @@ ax.set(xlabel="Day",
 # Rotate tick marks on x-axis
 plt.setp(ax.get_xticklabels(), rotation=45)
 
-plt.show()
-
 st.header("Performance for the first five weeks :")
-st.markdown("The first 5 weeks have seen a total of %i customers visiting either for an in-house or a takeaway order. These have contributed to the total revenue of %i $. The average bill amount per customer comes out to be %i $." %(l2, a4 , a4/l2))
+st.markdown("The first 5 weeks have seen a total of %i customers visiting either for an in-house or a takeaway order. These have contributed to the total revenue of %i \\$. The average bill amount per customer comes out to be %i \\$." %(l2, a4 , a4/l2))
 st.write(fig1)
-st.markdown("The highest single day revenue was recorded on Tuesday, 7th July. New menu items were also introduced on this day. Though these new menu items were not the most ordered dishes, they had customers explore the previously less demanded items on the menu. Another day which saw above average performance was 21st July. The Men's and Ladies feature night menu introduced in the previous week (14th July) were in demand on the day, contributing to half of the revenue generated. These experiments clearly seem to be working for the restaurant. The average revenue per day of 784 $ saw an improvement of 461 $ as the average revenue on the three days on which the retaurant experimented with the menu was 1245 $. Introducing such changes every month will result in an increased business by 8%.")
+st.markdown("The highest single day revenue was recorded on Tuesday, 7th July. New menu items were also introduced on this day. Though these new menu items were not the most ordered dishes, they had customers explore the previously less demanded items on the menu. Another day which saw above average performance was 21st July. The Men's and Ladies feature night menu introduced in the previous week (14th July) were in demand on the day, contributing to half of the revenue generated. These experiments clearly seem to be working for the restaurant. The average revenue per day of 784 \\$ saw an improvement of 461 \\$ as the average revenue on the three days on which the retaurant experimented with the menu was 1245 \\$. Introducing such changes every month will result in an increased business by 8%.")
 st.write(fig2)
 st.markdown("The revenue sees a steady and consistent growth through the five weeks")
 st.write(fig3)
@@ -109,26 +105,26 @@ st.markdown("While the total revenues on Thursday is the lowest, it is important
 ####
 df2 = data[['Order_ID', 'SERVER','TOTAL']]
 group = df2.groupby('Order_ID')
-df3 = group['TOTAL'].agg([np.sum])
+df3 = group['TOTAL'].agg(['sum'])
 df4 = df2[['Order_ID','SERVER']]
 df4 = df4.drop_duplicates()
 group2 = df2.groupby('SERVER')
-df5 = group2['TOTAL'].agg([np.sum])
+df5 = group2['TOTAL'].agg(['sum'])
 df5['SERVER_NAME'] = df5.index
 df6 = df4['SERVER'].value_counts()
 df6.columns = ['server_name','num_of_bills']
 df6 = df6.to_frame()
 df6['SERVER_NAME']=df6.index
 df7 = pd.merge(df5, df6, on='SERVER_NAME', how='outer')
-df7['avg_bill'] = df7['sum']/df7['SERVER']
-df7 = df7.rename(columns={"sum": "Total revenue generated", "SERVER_NAME": "Server Name", "SERVER" : "Customers served", "avg_bill" : "Average revenue per bill"})
+df7['avg_bill'] = df7['sum']/df7['count']
+df7 = df7.rename(columns={"sum": "Total revenue generated", "SERVER_NAME": "Server Name", "count" : "Customers served", "avg_bill" : "Average revenue per bill"})
 df7.sort_values(by='Average revenue per bill', ascending=False)
 q = df7.sort_values(by='Average revenue per bill', ascending=False)
 w = df7.sort_values(by='Total revenue generated', ascending = False)
 
 df9 = data[['DATE','TOTAL']]
 a = df9.groupby('DATE')
-df9 = a['TOTAL'].agg([np.sum])
+df9 = a['TOTAL'].agg(['sum'])
 df10 = data[['DATE','Order_ID']]
 b = df10.groupby('DATE')
 df10 = b['Order_ID'].nunique()
@@ -144,7 +140,7 @@ select = st.selectbox('Show the list of servers by:', ['Efficiency', 'Revenue ge
 if select == 'Efficiency':
         st.write(q)
         st.markdown("Standard deviation for average revenue generated everyday is %i dollars" %(a))
-        st.markdown("The standard deviation in the average revenue generated by the servers is %i. (Ignoring outlier server name Bailey T.) The total number of customers served by the server's with low average revenue generated is %i. (Contributing to negative standard deviation.) Replacing these servers with the server's with higher average revenue or awarding performance based incentives will see an improved business as follows : %i * %i = $ %i i.e. %i percent increase in revenue." %(a, b , a,  b , a * b , ((a * b)/c) * 100)) 
+        st.markdown("The standard deviation in the average revenue generated by the servers is %i. (Ignoring outlier server name Bailey T.) The total number of customers served by the server's with low average revenue generated is %i. (Contributing to negative standard deviation.) Replacing these servers with the server's with higher average revenue or awarding performance based incentives will see an improved business as follows : %i * %i = \\$ %i i.e. %i percent increase in revenue." %(a, b , a,  b , a * b , ((a * b)/c) * 100)) 
 if select == 'Revenue generated':
         st.write(df7.sort_values(by='Total revenue generated', ascending=False))
         st.markdown("Deb, Jillian and Ashley have attended %i of the total %i customers in the month, contributing to more than half of the revenue generated. Jillian has a higher average revenue per bill" %(w['Customers served'][0:3].sum() , w['Customers served'].sum()))
@@ -164,13 +160,13 @@ d13 = d13.to_frame()
 d13.index.names = ['DAY']
 d13.columns = ['DAYS']
 group4 = d12.groupby('DAY')
-d14 = group4['TOTAL'].agg([np.sum])
+d14 = group4['TOTAL'].agg(['sum'])
 e = pd.merge(d14, d13, on='DAY', how='outer')
 e['Average day of week'] = e['sum']/e['DAYS']
 e = e.loc[['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'], :]
 e.columns = ['Total revenue for this day', 'Number of working days', 'Average revenue']
-a1 = (e['Average revenue'][1] + e['Average revenue'][3] + e['Average revenue'][4:].sum())/5
-a2 = (e['Average revenue'][0] + e['Average revenue'][2])/2 
+a1 = (e['Average revenue'].iloc[1] + e['Average revenue'].iloc[3] + e['Average revenue'].iloc[4:].sum())/5
+a2 = (e['Average revenue'].iloc[0] + e['Average revenue'].iloc[2])/2 
 a3 = a1 - a2
 a4 = data['TOTAL'].sum()
 a5 = ((a3 * 4) / (data['TOTAL'].sum())) * 100
@@ -198,7 +194,7 @@ ax.xaxis.set_tick_params(pad = 5)
 ax.yaxis.set_tick_params(pad = 10) 
   
 # Add x, y gridlines 
-ax.grid(b = True, color ='grey', 
+ax.grid(True, color ='grey', 
         linestyle ='-.', linewidth = 0.5, 
         alpha = 0.2) 
   
@@ -222,34 +218,31 @@ fig.text(0.9, 0.15, 'Rohit Bhalerao', fontsize = 12,
          alpha = 0.7) 
   
 # Show Plot 
-plt.show() 
 st.write(fig)
 
 st.write(e) 
-st.markdown("Even though Tuesdays and Thursdays see a good business than other days of the week, the restaurant was closed on 2 Tuesdays and 3 Thursdays in the 5 weeks. So, it is suggested to change this, and to keep the restaurant closed on either Monday's or Wednesday's for Sanitization and Maintenance purpose. The average business of Tuesdays, Thursdays, Fridays and the weekends is %i $. The average business of Wednesdays and Mondays is %i $.This action will add up the revenue by around %i $ every week, i.e. , %i $ a month. Business performance will improve by around %i percent." %(a1, a2 , a3 , a3 * 4 , a5))
+st.markdown("Even though Tuesdays and Thursdays see a good business than other days of the week, the restaurant was closed on 2 Tuesdays and 3 Thursdays in the 5 weeks. So, it is suggested to change this, and to keep the restaurant closed on either Monday's or Wednesday's for Sanitization and Maintenance purpose. The average business of Tuesdays, Thursdays, Fridays and the weekends is %i \\$. The average business of Wednesdays and Mondays is %i \\$.This action will add up the revenue by around %i \\$ every week, i.e. , %i \\$ a month. Business performance will improve by around %i percent." %(a1, a2 , a3 , a3 * 4 , a5))
 
 ####
 z = data[['DAY' , 'ITEM','FOOD TYPE', 'WEEK NUMBER']]
 groupz = z.groupby('WEEK NUMBER')
 z1 = groupz['ITEM'].value_counts()
-z1 = z1.to_frame()
-z2 = z1['ITEM']['WEEK 1'][0:5]
-z3 = z1['ITEM']['WEEK 2'][0:5]
-z4 = z1['ITEM']['WEEK 3'][0:5]
-z5 = z1['ITEM']['WEEK 4'][0:5]
-z6 = z1['ITEM']['WEEK 5'][0:5]
+z2 = z1['WEEK 1'][0:5]
+z3 = z1['WEEK 2'][0:5]
+z4 = z1['WEEK 3'][0:5]
+z5 = z1['WEEK 4'][0:5]
+z6 = z1['WEEK 5'][0:5]
 
 z7 = data[['DAY' , 'ITEM','FOOD TYPE']]
 groupz2 = z7.groupby('DAY')
 z8 = groupz2['ITEM'].value_counts()
-z8 = z8.to_frame()
-z9 = z8['ITEM']['MONDAY'][0:5]
-z10 = z8['ITEM']['TUESDAY'][0:5]
-z11 = z8['ITEM']['WEDNESDAY'][0:5]
-z12 = z8['ITEM']['THURSDAY'][0:5]
-z13 = z8['ITEM']['FRIDAY'][0:5]
-z14 = z8['ITEM']['SATURDAY'][0:5]
-z15 = z8['ITEM']['SUNDAY'][0:5]
+z9 = z8['MONDAY'][0:5]
+z10 = z8['TUESDAY'][0:5]
+z11 = z8['WEDNESDAY'][0:5]
+z12 = z8['THURSDAY'][0:5]
+z13 = z8['FRIDAY'][0:5]
+z14 = z8['SATURDAY'][0:5]
+z15 = z8['SUNDAY'][0:5]
 
 df_final=pd.DataFrame(data["FOOD TYPE"])
 df_final['ITEM']=data["ITEM"].values
